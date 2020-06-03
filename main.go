b8d3283b68e865567a8b81ec6928b498aabfe135
Kenneth Ratliff <kratliff@uber.com>
2020-06-03 16:15:07 -0500

    Added make file

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"./api"
 )
 
 func main() {
+	bindAddr := flag.String("bind_addr", ":8080", "Set bind address")
+	flag.Parse()
 	a := api.New(":9111")
 	log.Fatal(a.Start())
 }
